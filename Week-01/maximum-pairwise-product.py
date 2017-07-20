0aea52ec44abf6a80a19508d340541cd97df349e
winfredselwyn <winfredselwyn@gmail.com>
2017-07-20 22:48:04 +0800

    Completed Week One's algorithm problem - Maximum Pairwise Product with Python.

--- Week-01/maximum-pairwise-product.py
+++ Week-01/maximum-pairwise-product.py
@@ -1,14 +1,11 @@
-# Uses python3
 n = int(input())
-a = [int(x) for x in input().split()]
-assert(len(a) == n)
+myList = [int(x) for x in input().split(" ")]
+assert(len(myList) == n)
 
 result = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
-
-print(result)
-# print(n)
+def pairWise():
+    sortedArray = sorted(myList)
+    print(sortedArray[-1] * sortedArray[-2])
+    
+pairWise()
